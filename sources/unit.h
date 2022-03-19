5ae2daeb08f2e6480e8d05a728c0ccc4730d389b
dogpie <dankov.ns@phystech.edu>
2022-03-19 19:50:41 +0300

    hp bar(veryyy raw) and some cleaning up

--- sources/unit.h
+++ sources/unit.h
@@ -2,13 +2,15 @@
 #define GAME_ON_SFML_SOURCES_OBJECT_H_
 
 #include "SFML/Graphics.hpp"
-#include "graphics.h"
+#include "dynamic_graphics.h"
 #include "mortal.h"
 
 class Unit : public Mortal {
  public:
+  bool is_attacking_;
+  int damage_;
+
   float speed_;
-  bool is_dead_, is_full_dead_;
 
   Unit() = default;
 
