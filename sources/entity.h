853d06708ce91c32dfcf8da87d979d5c750bb8cc
valkon29 <konovalov.vr@phystech.edu>
2022-03-19 03:26:03 +0300

    Edited entity.h

--- sources/entity.h
+++ sources/entity.h
@@ -1,22 +1,19 @@
 #ifndef GAMEPROJECTTP_SOURCES_ENTITY_H_
 #define GAMEPROJECTTP_SOURCES_ENTITY_H_
 
-#include "graphics.h"
 #include "SFML/Graphics.hpp"
+#include "graphics.h"
 
 class Entity {
  public:
   float x_, y_, h_, w_;
-  Graphics graphics_;
+  DynamicGraphics graphics_;
 
   Entity() = default;
 
-  void Draw(sf::RenderWindow &window) {
-    graphics_.Draw(window, x_, y_);
-  };
+  void Draw(sf::RenderWindow& window) { graphics_.Draw(window, x_, y_); };
 
   virtual void Update(float time) = 0;
-
 };
 
-#endif //GAMEPROJECTTP_SOURCES_ENTITY_H_
+#endif  // GAMEPROJECTTP_SOURCES_ENTITY_H_
