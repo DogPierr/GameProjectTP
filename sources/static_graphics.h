cfa37c00577d691dd662d3725b86c6a7d534f822
valkon29 <konovalov.vr@phystech.edu>
2022-03-19 03:25:26 +0300

    Add static_graphics.h

---
+++ sources/static_graphics.h
@@ -0,0 +1,23 @@
+#ifndef GAME_ON_SFML_STATIC_GRAPHICS_H
+#define GAME_ON_SFML_STATIC_GRAPHICS_H
+
+#include "SFML/Graphics.hpp"
+
+class Graphics {
+ public:
+  sf::Texture texture_;
+  sf::Sprite sprite_;
+  Graphics() = default;
+
+  Graphics(const std::string& image) {
+    texture_.loadFromFile(image);
+    sprite_.setTexture(texture_);
+  }
+
+  void Draw(sf::RenderWindow& window, float x, float y) {
+    sprite_.setPosition(x, y);
+    window.draw(sprite_);
+  }
+};
+
+#endif  // GAME_ON_SFML_STATIC_GRAPHICS_H
